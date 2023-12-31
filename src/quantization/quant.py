defe23a9d639e7759555ea6e1de5931715a49c22
Clyde013 <weipyn@outlook.com>
2023-12-31 16:38:34 +0800

    Implement quantization eval softertrainer

--- src/quantization/quant.py
+++ src/quantization/quant.py
@@ -1,10 +1,22 @@
 """
-Should contain quantization code for another branch. For now using placeholder
-functions for SofterTrainer implementation.
+Standalone quantization code. Contains a globally referencable dictionary of
+default quantization config settings and a function that will quantize a given model.
 """
+from transformers import PreTrainedModel, GPTQConfig
+from torch.utils.data import DataLoader
+
+# default quantization configuration, requires the dataset be specified
+default_quant_configs = {"bits": 4, "group_size": 128, "desc_act": False}
 
 
-def quantize(model):
-    # note: ensure model is returned on the device on which
-    # inference is to be run (GPU if supported, else CPU)
-    return model
+def quantize(model: PreTrainedModel, quant_config: GPTQConfig):
+    # quantization entry point in huggingface is done through the from_pretrained
+    # instantiation of a new model. Instead of saving the whole model to disk we just
+    # pass in the state dict of the existing model in memory to initialise a
+    # quantized version of it.
+    modelclass = model.__class__
+
+    quant_model = modelclass.from_pretrained(
+        state_dict=model.state_dict(), quantization_config=quant_config, device_map="auto"
+    )
+    return quant_model
--- src/training_utils.py
+++ src/training_utils.py
@@ -6,13 +6,26 @@ of the softermax models.
 from typing import Optional, List, Dict
 from torch.utils.data import Dataset
 
-from transformers import Trainer
+from dataclasses import dataclass, field
+from transformers import Trainer, TrainingArguments, GPTQConfig
 from transformers.utils import logging
 
-from .quantization.quant import quantize
+from .quantization.quant import quantize, default_quant_configs
 
 
 logger = logging.get_logger(__name__)
+
+
+@dataclass
+class SofterTrainingArguments(TrainingArguments):
+    """
+    Subclass of TrainingArguments to add a field for the quantization configuration
+    """
+
+    quant_kwargs: dict = field(
+        default_factory=default_quant_configs,
+        metadata={"help": "GPTQConfig-specific **kwargs"},
+    )
 
 
 class SofterTrainer(Trainer):
@@ -40,28 +53,34 @@ class SofterTrainer(Trainer):
         self.model.config.output_attentions = True
 
         # first run regular model eval
-        fp16_metrics = super().evaluate(eval_dataset, ignore_keys, metric_key_prefix="fp16")
+        print("regular model eval")
+        orig_metrics = super().evaluate(eval_dataset, ignore_keys, metric_key_prefix="orig")
 
         # store a ref to the original model while moving it to cpu
-        fp16_model = self.model.to("cpu")
+        orig_model = self.model.to("cpu")
         # quantize the model
+        print("quantizing")
         logger.info("Quantizing model for eval loop")
-        int8_model = quantize(self.model)
+        
+        quant_config = GPTQConfig(dataset=self.eval_dataset, **self.args.quant_kwargs)
+        quant_model = quantize(self.model, quant_config)
         # replace current ref to the quantized model
-        self.model = int8_model
+        self.model = quant_model
 
         # then run quant model eval
-        int8_metrics = super().evaluate(eval_dataset, ignore_keys, metric_key_prefix="int8")
+        print("quant model eval")
+        quant_metrics = super().evaluate(eval_dataset, ignore_keys, metric_key_prefix="quant")
 
         # return self.model ref to regular model
-        self.model = fp16_model
+        self.model = orig_model
         # reset output_attentions to its original value
         self.model.config.output_attentions = orig_output_attentions
 
         # combine both metric dictionaries into one
-        metrics_dict = fp16_metrics.update(int8_metrics)
+        metrics_dict = orig_metrics.update(quant_metrics)
         return metrics_dict
 
 
 def compute_softermetrics(eval_preds):
+    print("compute metrics")
     print(eval_preds)
